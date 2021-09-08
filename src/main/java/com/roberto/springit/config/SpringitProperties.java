9d5b1e84da464fb20a5f53f968ed097cbd5d3489
robertotambunan <robertotambunan14@gmail.com>
2021-09-08 21:58:35 +0700

    learning actuator

---
+++ src/main/java/com/roberto/springit/config/SpringitProperties.java
@@ -0,0 +1,22 @@
+package com.roberto.springit.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
+
+public class SpringitProperties {
+    
+    /**
+     * This is our welcome message
+     */
+    private String welcomeMsg = "Hello, World!";
+
+	public String getWelcomeMsg() {
+		return welcomeMsg;
+	}
+
+	public void setWelcomeMsg(String welcomeMsg) {
+		this.welcomeMsg = welcomeMsg;
+	}
+
+}
