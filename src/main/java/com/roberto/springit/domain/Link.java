c0964844079aca14c73207807440e6b190c4dde2
robertotambunan <robertotambunan14@gmail.com>
2021-09-13 00:06:07 +0700

    add mapping link and comment

--- src/main/java/com/roberto/springit/domain/Link.java
+++ src/main/java/com/roberto/springit/domain/Link.java
@@ -1,8 +1,12 @@
 package com.roberto.springit.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +23,11 @@ public class Link {
 	@NonNull
 	private String title;
 	@NonNull
-    private String url;
+	private String url;
+	
+	//comments
+	@OneToMany(mappedBy = "link")
+	private List<Comment> comments = new ArrayList<>();
 
 }
 
