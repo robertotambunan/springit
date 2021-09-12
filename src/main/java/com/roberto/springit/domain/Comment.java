8d09ea7580aa1599d94a5476ea6fb881dc483303
robertotambunan <robertotambunan14@gmail.com>
2021-09-12 23:36:51 +0700

    Created entities and refactor using Project Lombok

---
+++ src/main/java/com/roberto/springit/domain/Comment.java
@@ -0,0 +1,24 @@
+package com.roberto.springit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+
+}
+
+
+
